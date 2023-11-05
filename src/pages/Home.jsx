23d86a4ff1e60cb891fbe534a5212146aa60f2c7
Prateek <prateekkukreja@outlook.com>
2023-11-05 23:28:27 +0530

    added select kit component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import SelectKit from "../components/SelectKit";
 
 function Home() {
   return (
     <>
       <Hero />
+      <SelectKit />
     </>
   );
 }
