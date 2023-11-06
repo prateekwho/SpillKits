e2f561a3d88516a02d547110f9afba6a8cee3f18
Prateek <prateekkukreja@outlook.com>
2023-11-06 16:49:22 +0530

    added choose us component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import SelectKit from "../components/SelectKit";
 import Banner from "../components/Banner";
+import ChooseUs from "../components/ChooseUs";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Hero />
       <SelectKit />
       <Banner />
+      <ChooseUs />
     </>
   );
 }
