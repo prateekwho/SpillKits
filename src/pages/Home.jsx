fdcfcaed1e953f91ec10f94ac5085c43282a0287
Prateek <prateekkukreja@outlook.com>
2023-11-06 15:27:58 +0530

    added banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import SelectKit from "../components/SelectKit";
+import Banner from "../components/Banner";
 
 function Home() {
   return (
     <>
       <Hero />
       <SelectKit />
+      <Banner />
     </>
   );
 }
