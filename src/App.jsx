32027416752a0c44bc6f5757e475982b32a76cb0
Prateek <prateekkukreja@outlook.com>
2023-11-09 13:54:51 +0530

    added topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import TopBar from "./components/TopBar";
+
 function App() {
   return (
     <>
+      <TopBar />
       <Navbar />
       <Routes>
         <Route index path="/" element={<Home />} />
